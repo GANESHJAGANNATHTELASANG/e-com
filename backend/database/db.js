36b08f1845fa5ca03b671ffccc30feaab4af90f3
GANESHJAGANNATHTELASANG <ganesh.telasang8@gmail.com>
2026-08-31 14:32:27 +0530

    here we done the database connection code and successfully it is added next is the model creating

---
+++ backend/database/db.js
@@ -0,0 +1,18 @@
+import pkg from "pg";
+
+const { Client } = pkg;
+
+const database = new Client({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+});
+
+try {
+  await database.connect();
+  console.log("the database is connected");
+} catch (error) {
+  console.log("the error in the connecting in the database");
+}
